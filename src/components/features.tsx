d3fa8c14a30d179097dd2d264cde50ee1be12238
faizerShaikh <faizer.shaikh@apsissolutions.com>
2024-08-09 11:52:22 +0530

    added new changes

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -9,9 +9,9 @@ import {
 import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-const Skeleton = () => (
-  <div className='flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100'></div>
-);
+// const Skeleton = () => (
+//   <div className='flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100'></div>
+// );
 
 let cards: {
   name: string;
